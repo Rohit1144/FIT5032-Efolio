5860979f5e5d39b0d494be02162ba1bf3ef35ff2
Rohit8143 <rohitchowdary@controlshiftai.com>
2025-08-05 15:19:34 +1000

    added MyName.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,21 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+import MyName from './components/MyName.vue'
 import TheWelcome from './components/TheWelcome.vue'
-import JSON from './components/JSON.vue'
 </script>
 
 <template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <MyName msg="Hello, Rohit!" />
+      <HelloWorld msg="You did it!" />
+    </div>
+  </header>
+
   <main>
-    <JSON />
+    <TheWelcome />
   </main>
 </template>
 
